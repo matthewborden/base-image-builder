91e947b873c1b4888ae79eccebed1f0e6e9f0f13
Matthew Borden <matthew.b@buildkite.com>
2024-12-06 21:19:59 +1100

    Load image

--- scripts/build-base-image.sh
+++ scripts/build-base-image.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -e
+
 export GITHUB_TOKEN=$(buildkite-agent secret get GITHUB_TOKEN)
 REGISTRY="$(nsc workspace describe -o json -k registry_url)"
-IMAGE_NAME="$REGISTRY/base-image-$BUILDKITE_PIPLELINE_SLUG"
+IMAGE_NAME="$REGISTRY/base-image-$BUILDKITE_PIPELINE_SLUG"
 
 # Build the base image
 docker build --secret id=github_token,env=GITHUB_TOKEN \
+             --load \
              -t $IMAGE_NAME .
 
 docker save $IMAGE_NAME > base-image.tar
